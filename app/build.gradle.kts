e85da201fee1aec241a168ac8c819841ee30a430
x144k <x144k@users.noreply.github.com>
2026-05-06 18:43:22 -0400

    chore: bump version to 1.2.0 (3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.pureframe.exif"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
